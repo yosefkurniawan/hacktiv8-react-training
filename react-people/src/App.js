8699de26d250b423a3456c98473a5107346c9982
yosefkurniawan <jojo@icube.us>
2020-01-20 15:07:47 +0700

    course1_1_3 - Aliran Data dengan State dan Props

--- react-people/src/App.js
+++ react-people/src/App.js
@@ -25,31 +25,36 @@ function List(props) {
   );
 }
 
-function App() {
-  const human = [
-    { 
-      name: 'Kuririn',
-      thumbnail: 'https://upload.wikimedia.org/wikipedia/pt/6/63/Kuririn_42311.png',
-      power: 10
-    },
-    {
-      name: 'Bulma',
-      thumbnail: 'https://upload.wikimedia.org/wikipedia/hu/1/1c/Bulma.png',
-      power: 3
-    },
-    {
-      name: 'Chi-chi',
-      thumbnail:
-        'https://upload.wikimedia.org/wikipedia/hu/8/84/Chi-chi_dragon_ball_anime.jpg',
-      power: 30
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      human : [
+        {
+          name: 'Kuririn',
+          thumbnail: 'https://upload.wikimedia.org/wikipedia/pt/6/63/Kuririn_42311.png',
+          power: 10
+        },
+        {
+          name: 'Bulma',
+          thumbnail: 'https://upload.wikimedia.org/wikipedia/hu/1/1c/Bulma.png',
+          power: 3
+        },
+        {
+          name: 'Chi-chi',
+          thumbnail:
+            'https://upload.wikimedia.org/wikipedia/hu/8/84/Chi-chi_dragon_ball_anime.jpg',
+          power: 30
+        }
+      ]
     }
-  ]
-  return (
-    <>
+  }
+  render() {
+    return (
       <div className="App">
-        <List data={human} />
+        <List data={this.state.human} />
       </div>
-    </>
-  )
+    )
+  }
 }
 export default App;
