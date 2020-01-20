fc6b505f4fa5eaa971ca95c70f5d1fee6907bdc5
yosefkurniawan <jojo@icube.us>
2020-01-20 14:59:09 +0700

    course1_1_3 - Merapikan Aplikasi React People

--- react-people/src/App.js
+++ react-people/src/App.js
@@ -1,23 +1,54 @@
 import React from 'react';
+import './App.css';
 
-function UserInfo(props) {
+function sortByPower (a, b) {
+  return b.power - a.power;
+}
+
+function List(props) {
   return (
     <>
-      <p>
-        Username: {props.username}
-      </p>
-      <p>
-        Active: {props.active}
-      </p>
+      <h1>React People!</h1>
+      <ul>
+        {props.data.sort(sortByPower).map(person => (
+          <li key={person.name} className="list-item">
+            <div className="list-item-image-container">
+              <img src={person.thumbnail} />
+            </div>
+            <div className="list-item-name">
+              {person.name} (Power:{''} <span className="list-item-power">{person.power}</span>)
+            </div>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
 
 function App() {
+  const human = [
+    { 
+      name: 'Kuririn',
+      thumbnail: 'https://upload.wikimedia.org/wikipedia/pt/6/63/Kuririn_42311.png',
+      power: 10
+    },
+    {
+      name: 'Bulma',
+      thumbnail: 'https://upload.wikimedia.org/wikipedia/hu/1/1c/Bulma.png',
+      power: 3
+    },
+    {
+      name: 'Chi-chi',
+      thumbnail:
+        'https://upload.wikimedia.org/wikipedia/hu/8/84/Chi-chi_dragon_ball_anime.jpg',
+      power: 30
+    }
+  ]
   return (
     <>
-      <UserInfo username={'Yosef Kurniawan'} active={true}/>
-      <UserInfo username={'Vinsensius Niko'} active={false} />
+      <div className="App">
+        <List data={human} />
+      </div>
     </>
   )
 }
