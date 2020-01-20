ba78fc01868553c47b1b8ec32471d073e3573680
Yosef Kurniawan <yosefkurniawan@Yosefs-MacBook-Pro.local>
2020-01-20 14:00:31 +0700

    course1_1_2 - Membangun User Interface dengan JSX

--- react-people/src/App.js
+++ react-people/src/App.js
@@ -1,19 +1,32 @@
 import React from 'react';
 
-function App() {
-  const people = [
-    { name: "Yosef Kurniawan" },
-    { name: "Vinsensius Niko" },
-    { name: "Abdurahman Ragum" }
-  ]
+function List(props) {
   return (
     <>
       <h1>React People!</h1>
       <ul>
-        {people.map(person => <li key={person.name}>{person.name}</li>)}
+        {props.data.map(person => <li key={person.name}>{person.name}</li>)}
       </ul>
     </>
   );
 }
 
+function App() {
+  const saiyan = [
+    { name: "Son Go Ku" },
+    { name: "Bezita" },
+    { name: "Son Gohan" }
+  ];
+  const human = [
+    { name: "Kuririn" },
+    { name: "Bulma" },
+    { name: "Chi-chi" }
+  ]
+  return (
+    <>
+      <List data={human} />
+      <List data={saiyan} />
+    </>
+  )
+}
 export default App;
