fe04dbdef487e8e55bcc25c4db10fd236b6c3a0f
yosefkurniawan <jojo@icube.us>
2020-02-25 16:53:15 +0700

    course2.1 - implement testing code of implementing redux

--- vanilla-redux/src/App.js
+++ vanilla-redux/src/App.js
@@ -1,24 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createStore } from 'redux';
+
+// Reducer
+function counterReducer(state = 0, action) {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state
+  }
+}
+
+// Store
+const store = createStore(counterReducer);
+
+// Actions
+function incrementCounter() {
+  return {
+    type: 'INCREMENT'
+  }
+}
+function decrementCounter() {
+  return {
+    type: 'DECREMENT'
+  }
+}
+
+// Initial State
+console.log(`Initial State: ${store.getState()}`);
+
+// Subscriber
+store.subscribe(function () {
+  console.log(`Initial changed: ${store.getState()}`);
+});
+
+// Dispatching an action
+store.dispatch(incrementCounter()); // 1
+store.dispatch(incrementCounter()); // 2
+store.dispatch(decrementCounter()); // 1
+store.dispatch(incrementCounter()); // 3
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      See the console log for the testing...
     </div>
   );
 }
