14d116334d66d5aaf3b80eca654f6d033754a2a2
yosefkurniawan <jojo@icube.us>
2020-03-05 16:19:31 +0700

    course2.2 - sample field level validation with formik

--- react-formik/src/App.js
+++ react-formik/src/App.js
@@ -2,13 +2,15 @@ import React, { useState } from 'react';
 import Simpleform from './simple-form';
 import Simpleformik from './simple-formik';
 import Formikyup from './formik-yup';
+import FieldLevelValidation from './formik-field-validation';
 
 function App() {
   return (
     <div className="App">
       {/* <Simpleform/> */}
-      <Simpleformik />
-      <Formikyup/>
+      {/* <Simpleformik /> */}
+      {/* <Formikyup/> */}
+      <FieldLevelValidation />
     </div>
   );
 }
