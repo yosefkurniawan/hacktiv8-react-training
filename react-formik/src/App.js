78f1142708ce913c4cc5ac7323318e1266833323
yosefkurniawan <jojo@icube.us>
2020-03-05 09:57:26 +0700

    course2.2 - sample form with simple formik

--- react-formik/src/App.js
+++ react-formik/src/App.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import Simpleform from './simple-form';
+import Simpleformik from './simple-formik';
 
 function App() {
   return (
     <div className="App">
-      <Simpleform/>
+      {/* <Simpleform/> */}
+      <Simpleformik/>
     </div>
   );
 }
