e661dbaaee6a7d486bc2def1e69fed4f8d3b3bf6
yosefkurniawan <jojo@icube.us>
2020-03-05 10:52:34 +0700

    course2.2 - sample sync and async validation with formik

--- react-formik/src/simple-formik.js
+++ react-formik/src/simple-formik.js
@@ -3,20 +3,50 @@
 import React from 'react';
 import {Formik, Form, Field, ErrorMessage} from 'formik';
 
+// Synchronus Validation
+const syncValidate = (
+    values, 
+    props   /* only available when using withFormik */
+) => {
+    const errors = {};
+    if (!values.email) {
+        errors.email = "Required"
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+        errors.email = "Invalid Email Address";
+    }
+    if (!values.username) {
+        errors.username = "Required"
+    }
+    return errors;
+}
+
+// Aynchronus Validation
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+const asyncValidate = (
+    values, 
+    props   /* only available when using withFormik */
+) => {
+    return sleep(2000).then(() => {
+        const errors = {};
+        if (!values.email) {
+            errors.email = "Required"
+        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+            errors.email = "Invalid Email Address";
+        }
+        if (!values.username) {
+            errors.username = "Required"
+        }
+        return errors;
+    });
+}
+
 const Simpleformik = () => (
     <div>
         <h1>Any place in your app!</h1>
         <Formik
-            initialValues={{email: "", password: ""}}
-            validate={values => {
-                const errors = {};
-                if(!values.email) {
-                    errors.email = "Required"
-                }else if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)){
-                    errors.email = "Invalid Email Address";
-                }
-                return errors;
-            }}
+            initialValues={{ email: "", username: "", password: ""}}
+            validate={(values, props) => syncValidate(values, props)}
+            // validate={(values, props) => asyncValidate(values, props)}
             onSubmit={(values, {setSubmitting}) => {
                 setTimeout(() => {
                     alert(JSON.stringify(values, null, 2));
@@ -37,6 +67,8 @@ const Simpleformik = () => (
                 <Form>
                     <Field type="email" name="email" />
                     <ErrorMessage name="email" component="div" />
+                    <Field type="text" name="username" />
+                    <ErrorMessage name="username" component="div" />
                     <Field type="password" name="password" />
                     <ErrorMessage name="password" component="div" />
                     <button type="submit" disabled={isSubmitting}>
